813a83830713bda725995a4b3a1fe1857bab1c9c
Takahiro Kamiya <only@jp.bigplanet.com>
2015-10-19 15:51:31 +0900

    分析機能の追加

--- js/esri-leaflet.js
+++ js/esri-leaflet.js
@@ -1,13 +1,15 @@
 
-	var map, basemapLayer, amenityLayer, amenityClusterLayer, amenityHeatmapLayer;
+	var map, basemapLayer, amenityLayer, amenityClusterLayer, amenityHeatmapLayer, circleLayer, marker;
+	var enable_click, click_flg;
+	var features = [];
+    var circleMarkers = [];
+    var sizuoka_osm_point_url = 'http://services.arcgis.com/wlVTGRSYTzAbjjiC/arcgis/rest/services/sizuoka_osm_point/FeatureServer/0';
 
     function init() {
 	
 		basemapLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		                                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 		                   });
-
-	    var sizuoka_osm_point_url = 'http://services.arcgis.com/wlVTGRSYTzAbjjiC/arcgis/rest/services/sizuoka_osm_point/FeatureServer/0';
 
 	    amenityLayer = L.esri.featureLayer({
 	                                  url: sizuoka_osm_point_url,
@@ -30,7 +32,7 @@
 	    // ヒートマップ
 	    amenityHeatmapLayer = L.esri.heatmapFeatureLayer({
 					                  url: sizuoka_osm_point_url,
-					                  radius: 50
+					                  radius: 40
 					              });
 
 	    map = L.map('map', {
@@ -38,6 +40,8 @@
 	              });
 		
 		map.setView([34.98, 138.38], 10);
+
+		click_flg = false;
 
 	}
 
@@ -99,6 +103,7 @@
     
     // 検索
     $("#amenityBtn").click(function() {
+
         if ($('#amenity').is(':checked')) {
             // mask
             blockUI();
@@ -110,14 +115,15 @@
             	                      );
 	        } else {
 	     	    amenityLayer.setWhere('amenity=\'' + $("#select_amenity").val() + '\'', 
-	     	                              function() {
+	     	                              function() { 
                                              $.unblockUI(); 
                                           }
                                       );
 	        }
 	    }
     });
-
+    
+    // クラスター
     $('#clustered').change(function() {
        if ($(this).is(':checked')) {
          map.addLayer(amenityClusterLayer);
@@ -125,7 +131,8 @@
          map.removeLayer(amenityClusterLayer);
        }
     });
-
+    
+    // ヒートマップ
     $('#heatmap').change(function() {
        if ($(this).is(':checked')) {
          map.addLayer(amenityHeatmapLayer);
@@ -133,4 +140,127 @@
          map.removeLayer(amenityHeatmapLayer);
        }
     });
+    
+    // エリア作成
+    $("#areaBtn").click(function() {
+         var radius = $("#radius").val() * 1000;
+         if (marker) {
+            var latlng = marker.getLatLng();
+			if (circleLayer) {
+			   map.removeLayer(circleLayer);
+			}
+			circleLayer = L.circle([latlng.lat, latlng.lng], radius, {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.3
+			}).addTo(map);
+         }
+         /*
+         amenityLayer.query()
+		     .where('amenity=\'' + $("#select_amenity").val() + '\'')
+		     .run(function(error, featureCollection){
+					for (var i = 0; i < featureCollection.features.length; i++) {
+						var x = featureCollection.features[i].geometry.coordinates[0];
+						var y = featureCollection.features[i].geometry.coordinates[1];
+						circleLayer = L.circle([y, x], radius, {
+                             color: 'red',
+                             fillColor: '#f03',
+                             fillOpacity: 0.5
+						}).addTo(map);
+					}		 
+		 });
+         */
+    });
+
+    // クリックイベント　
+    $("#pointBtn").click(function() {
+        if (click_flg === false) {
+            map.on('click', onClick);
+    	    click_flg = true;
+        } else if (click_flg === true) {
+            map.off('click', onClick);
+            click_flg = false;
+        }
+    });
+
+    // 地点作成
+    function onClick(e) {
+    	if (marker) {
+           map.removeLayer(marker);    	
+		}
+		var greenIcon = L.icon({
+		    iconUrl: 'http://leafletjs.com/docs/images/leaf-green.png',
+		    shadowUrl: 'http://leafletjs.com/docs/images/leaf-shadow.png',
+
+		    iconSize:     [38, 95], // size of the icon
+		    shadowSize:   [50, 64], // size of the shadow
+		    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+		    shadowAnchor: [4, 62],  // the same for the shadow
+		    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+		});
+        marker = new L.Marker(e.latlng, {icon: greenIcon, draggable:true});
+        map.addLayer(marker);
+
+        map.off('click', onClick);
+        click_flg = false;
+    }
+
+    // クリア
+    $("#areaClearBtn").click(function() {
+        if (circleLayer) {
+          map.removeLayer(circleLayer);
+        }
+        if (marker) {
+          map.removeLayer(marker);
+
+        }
+        if (circleMarkers.length > 0) {
+	        for (var i = 0; i < circleMarkers.length; i++) {
+	            map.removeLayer(circleMarkers[i]);
+	        }
+            $(".number_title").text("0");
+    	}
+    });
+    
+    // エリア内分析
+    $("#areaAnalysisBtn").click(function() {
+		var query = L.esri.Tasks.query({
+		    url: sizuoka_osm_point_url
+		});
+        var radius = $("#radius").val() * 1000;
+        if (marker) {
+            // mask
+            blockUI();
+            var latlng = marker.getLatLng();
+            if (circleMarkers.length > 0) {
+		        for (var i = 0; i < circleMarkers.length; i++) {
+		            map.removeLayer(circleMarkers[i]);
+		        }
+                circleMarkers = [];
+        	}
+            query.nearby(latlng, radius);
+            query.run(function(error, featureCollection, response){
+			    //console.log('Found ' + featureCollection.features.length + ' features');
+				for (var i = 0; i < featureCollection.features.length; i++) {
+					var x = featureCollection.features[i].geometry.coordinates[0];
+					var y = featureCollection.features[i].geometry.coordinates[1];
+					var latlng = L.latLng(y, x);
+	                circleMarker = L.circleMarker(latlng, {
+												color: '#22B14C',
+												weight: 2,
+												opacity: 0.85,
+												fillOpacity: 0.5
+				    }).bindPopup("Amenity: " + featureCollection.features[i].properties.amenity + "<br/>Name: " + featureCollection.features[i].properties.name);
+                    circleMarkers.push(circleMarker);
+                }
+                for (var i = 0; i < circleMarkers.length; i++) {
+                   map.addLayer(circleMarkers[i]);
+                }
+                $(".number_title").text(circleMarkers.length);
+                $.unblockUI();
+            });
+		}
+
+    });
+
 
